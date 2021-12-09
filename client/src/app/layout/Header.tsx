3745cc7bbba9373c95e6b78c29a6dd01c02ffd8e
Mikael Andres <msandres123@gmail.com>
2021-12-09 17:34:31 -0500

    End of Section 9

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,18 @@
 import { ShoppingCart } from '@mui/icons-material';
-import {AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import { Link, NavLink } from 'react-router-dom';
 import { useAppSelector } from '../store/configureStore';
+import SignedInMenu from './SignedInMenu';
 
 interface Props {
   darkMode: boolean;
@@ -32,50 +43,66 @@ const navStyles = {
 };
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
-  const {basket} = useAppSelector(state => state.basket)
-  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
+  const { basket } = useAppSelector((state) => state.basket);
+  const { user } = useAppSelector((state) => state.account);
+  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <Box display='flex' alignItems='center'>
-        <Typography
-          variant="h6"
-          component={NavLink}
-          sx={navStyles}
-          exact
-          to="/"
-        >
-          Re-Store
-        </Typography>
-        <Switch checked={darkMode} onChange={handleThemeChange} />
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography
+            variant="h6"
+            component={NavLink}
+            sx={navStyles}
+            exact
+            to="/"
+          >
+            Re-Store
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange} />
         </Box>
         <Box>
-        <List sx={{ display: 'flex' }}>
-          {midLinks.map(({ title, path }) => (
-            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-              {title.toLocaleUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+          <List sx={{ display: 'flex' }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toLocaleUpperCase()}
+              </ListItem>
+            ))}
+          </List>
         </Box>
-        <Box display='flex' alignItems='center'>
-        <IconButton component={Link} to="/basket" size="large" sx={{ color: 'inherit' }}>
-          <Badge badgeContent={itemCount} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
-        <List sx={{ display: 'flex' }}>
-          {rightLinks.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={path}
-              sx={navStyles}
-            >
-              {title.toLocaleUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+        <Box display="flex" alignItems="center">
+          <IconButton
+            component={Link}
+            to="/basket"
+            size="large"
+            sx={{ color: 'inherit' }}
+          >
+            <Badge badgeContent={itemCount} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          {user ? (
+            <SignedInMenu />
+          ) : (
+            <List sx={{ display: 'flex' }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toLocaleUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
