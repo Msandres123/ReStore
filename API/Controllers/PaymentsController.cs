ef6ddf19190f7c220a502228418da080f7798d2e
Mikael Andres <msandres123@gmail.com>
2021-12-20 19:53:35 -0500

    payments controller updated

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -58,14 +58,14 @@ namespace API.Controllers
         public async Task<ActionResult> StripeWebhook()
         {
             var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            
-            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], 
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
                 _config["StripeSettings:WhSecret"]);
 
             var charge = (Charge)stripeEvent.Data.Object;
 
             var order = await _context.Orders.FirstOrDefaultAsync(x => 
-            x.PaymentIntentId == charge.PaymentIntentId);
+                x.PaymentIntentId == charge.PaymentIntentId);
 
             if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
 
